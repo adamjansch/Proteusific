ccfd86e1e13c9cfbe39cd2d791d25d198bf2f889
Adam Jansch <adam@adamjansch.co.uk>
2020-12-06 21:27:52 +0000

    1 - Retrieve MIDI device info
    Grabing MIDI device info via Core MIDI.

--- Proteusific/Global/ProteusificApp.swift
+++ Proteusific/Global/ProteusificApp.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 @main
 struct ProteusificApp: App {
+	// MARK: - PROPERTIES
+	// MARK: Stored properties
 	let persistenceController = PersistenceController.shared
 	
+	// MARK: Computed properties
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
 				.environment(\.managedObjectContext, persistenceController.container.viewContext)
 		}
+	}
+	
+	
+	// MARK: - METHODS
+	// MARK: Initializers
+	init() {
+		print("Available devices: \(MIDIManager.shared.availableDevices.compactMap({ $0.name }))")
 	}
 }
