09bd90ca2dfda1ac1911c29780f4311cb0d390e4
Adam Jansch <adam@adamjansch.co.uk>
2020-12-06 12:05:05 +0000

    Initial Commit

---
+++ Proteusific/ProteusificApp.swift
@@ -0,0 +1,20 @@
+//
+//  ProteusificApp.swift
+//  Proteusific
+//
+//  Created by Adam Jansch on 06/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ProteusificApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
