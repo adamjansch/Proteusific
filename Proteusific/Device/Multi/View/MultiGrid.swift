cf32c05ce52aeac0b885cd663e108627dbda0dd3
Adam Jansch <adam@adamjansch.co.uk>
2021-01-04 10:24:06 +0000

    7 - Send Preset Dump message
    Set up individual Device Detail tab views.

---
+++ Proteusific/Device/Multi/View/MultiGrid.swift
@@ -0,0 +1,20 @@
+//
+//  MultiGrid.swift
+//  Proteusific
+//
+//  Created by Adam Jansch on 04/01/2021.
+//
+
+import SwiftUI
+
+struct MultiGrid: View {
+	var body: some View {
+		Text("Multis")
+	}
+}
+
+struct MultiGrid_Previews: PreviewProvider {
+	static var previews: some View {
+		MultiGrid()
+	}
+}
---
+++ Proteusific/Device/Patch/View/PatchGrid.swift
@@ -0,0 +1,20 @@
+//
+//  PatchGrid.swift
+//  Proteusific
+//
+//  Created by Adam Jansch on 04/01/2021.
+//
+
+import SwiftUI
+
+struct PatchGrid: View {
+	var body: some View {
+		Text("Patches")
+	}
+}
+
+struct PatchGrid_Previews: PreviewProvider {
+	static var previews: some View {
+		PatchGrid()
+	}
+}
